da2e8e4ca18d3d91700ab90c1009703a42202aba
Arpita <arpitajain0811@gmail.com>
2018-02-27 19:47:14 +0530

    initial boilerplate

---
+++ src/render.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const Render = () => { ReactDOM.render(<App />, document.getElementById('root')); };
+export default Render;
